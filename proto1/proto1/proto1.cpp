03b2ada1b94ce20cdbff700ba3ea78f3591cb590
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2023-02-02 00:36:44 +0100

    proto1: added sfml and other libs, displays a window

--- proto1/proto1/proto1.cpp
+++ proto1/proto1/proto1.cpp
@@ -1,15 +1,39 @@
-#include <iostream>
-#include <proto1-model/proto1-model.hpp>
-
-int main (int argc, char* argv[])
+#include <SFML/Graphics.hpp>
+int main()
 {
-  using namespace std;
-
-  if (argc < 2)
-  {
-    cerr << "error: missing name" << endl;
-    return 1;
-  }
-
-  proto1_model::say_hello(cout, argv[1]);
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+    // // Load a sprite to display
+    // sf::Texture texture;
+    // if (!texture.loadFromFile("cute_image.jpg"))
+    //     return EXIT_FAILURE;
+    // sf::Sprite sprite(texture);
+    // // Create a graphical text to display
+    // sf::Font font;
+    // if (!font.loadFromFile("arial.ttf"))
+    //     return EXIT_FAILURE;
+    // sf::Text text("Hello SFML", font, 50);
+    // // Load a music to play
+    
+    // Start the game loop
+    while (window.isOpen())
+    {
+        // Process events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // Close window: exit
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        // Clear screen
+        window.clear();
+        // Draw the sprite
+        // window.draw(sprite);
+        // // Draw the string
+        // window.draw(text);
+        // Update the window
+        window.display();
+    }
+    return EXIT_SUCCESS;
 }
