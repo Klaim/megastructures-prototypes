68a3c02f2d7c0afd9a94072daa9efd275ef21476
Joël Lamotte (Klaim) <mjklaim@gmail.com>
2023-02-06 20:27:37 +0100

    Fixed doctest usage.
    - doctest is a header-only lib, so just using `lib{doctest}` and not `liba{doctest}` is the right way to use it;
    - to let doctest define the `main()` there was a macro to use;

--- proto1-model/tests/basics/driver.cpp
+++ proto1-model/tests/basics/driver.cpp
@@ -1,9 +1,11 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
 
 #include <proto1-model/version.hpp>
 #include <proto1-model/proto1-model.hpp>
 
-// #include <doctest/doctest.h>
 
-int main ()
+TEST_CASE( "dummy test" )
 {
+    CHECK( 1 == 1 );
 }
