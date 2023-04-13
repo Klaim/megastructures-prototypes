9ea131c128e06abf06f96a1d8ca6b4ab9fb3bcaa
Joël Lamotte (Klaim) <joel.lamotte@quantstack.net>
2023-04-13 20:55:23 +0200

    Workaround random generator init error

--- proto1-model/proto1-model/actors.cpp
+++ proto1-model/proto1-model/actors.cpp
@@ -11,8 +11,7 @@ namespace proto1::model::actors
     AnyAction random_action(ActionContext)
     {
         static std::random_device r;
-        static const std::seed_seq seed_seq {r(), r(), r(), r(), r(), r(), r(), r()};
-        static std::mt19937 rng_gen(seed_seq);
+        static std::mt19937 rng_gen(r()); // FIXME: better seed
         static const std::vector<AnyAction> possible_actions {
             actions::Move{ Vector2::LEFT },
             actions::Move{ Vector2::RIGHT },
